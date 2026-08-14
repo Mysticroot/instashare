9f983013a04111d260d82a45be01903850cc7f3c
Aashu <Aashupatil218@gmail.com>
2026-08-14 19:08:30 +0530

    Initial commit

--- .history/server_20260814190700.js
+++ .history/server_20260814190700.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN || "my_verify_token";
 
 app.get("/", (req, res) => {
   res.send("Instagram Reel POC is running");
